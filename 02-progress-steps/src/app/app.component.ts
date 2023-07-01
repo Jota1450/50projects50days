8b6fe0e2e3852c8cd1e29ccc562718e66039555b
Jorge Torres <jor-mario@hotmail.com>
2023-07-01 18:45:49 -0500

    02-progress-steps

---
+++ 02-progress-steps/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent implements OnInit {
+  title = '02-progress-steps';
+  circles: Array<HTMLFormElement> = [];
+  numOfActives = 1;
+
+  ngOnInit(): void {
+    this.circles = Array.from(document.querySelectorAll('.circle'));
+  }
+
+  prev() {
+    this.numOfActives--;
+    this.circles[this.numOfActives].classList.remove('active')
+  }
+
+  next() {
+    this.numOfActives++;
+    this.circles[this.numOfActives - 1].classList.add('active')
+  }
+
+  getProgress() {
+    return `${((this.numOfActives - 1) / (this.circles.length - 1)) * 100}%`
+  }
+}
